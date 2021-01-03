78687beb4848cc0b68e19e9831e4ac48e53dc5cf
Giuseppe Ingardia <g.ingardia@campus.unimib.it>
2021-01-03 21:29:59 +0100

    Modifiche layout
    rimossa la titlebar in ogni fragment

--- app/src/main/java/it/unimib/disco/sal/bestmovie/MainActivity.java
+++ app/src/main/java/it/unimib/disco/sal/bestmovie/MainActivity.java
@@ -1,6 +1,8 @@
 package it.unimib.disco.sal.bestmovie;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
